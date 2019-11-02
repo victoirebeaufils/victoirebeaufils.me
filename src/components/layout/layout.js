134c0ed370b4a226eab108d3981ad3c16ef010fa
Victoire Beaufils <victoire.beaufil@gmail.com>
2019-11-02 17:43:37 +0100

    Removed jquery and popper imports

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -3,9 +3,6 @@ import Header from '../header';
 import Footer from '../footer';
 import 'bootstrap/dist/css/bootstrap.css';
 import './layout.css';
-import $ from 'jquery';
-import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 function Layout (props){
     return (
         <div>
