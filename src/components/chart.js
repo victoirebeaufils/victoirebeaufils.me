891d8514ed7bb650584c24ed5a5d3000bd8c8610
Victoire Beaufils <victoire.beaufil@gmail.com>
2019-12-07 13:52:58 +0100

    Fixed highcharts build error

--- src/components/chart.js
+++ src/components/chart.js
@@ -3,6 +3,10 @@ import { render } from 'react-dom';
 import Highcharts from 'highcharts';
 import HighchartsReact from 'highcharts-react-official';
 import drilldown from 'highcharts-drilldown';
+import highchartsAccessibility from "highcharts/modules/accessibility";
+if (typeof window !== `undefined`) {
+  highchartsAccessibility(Highcharts);
+}
 drilldown(Highcharts);
 
 
