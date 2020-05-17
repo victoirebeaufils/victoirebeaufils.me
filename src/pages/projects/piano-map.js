500f786667ca7b68b43ac86dfba251ba1db04644
Victoire Beaufils <victoire.beaufil@gmail.com>
2020-05-17 17:55:19 +0200

    Added pianomap mockups

--- src/pages/projects/piano-map.js
+++ src/pages/projects/piano-map.js
@@ -10,33 +10,80 @@ import mockup3 from '../../images/pianomap/mockup3.png'
 import mockup4 from '../../images/pianomap/mockup4.png'
 
 const PianoMapPage = () => (
-  <Layout>
-      <ProjectBanner title="Piano map" github="https://www.github.com/victoirebeaufils/pianomap"
-      description="A map of public pianos around the world based on user contributions" illustration={pianoMapIcon}
-      status="Work in progress"
-      date="March 26, 2020">
+    <Layout>
+        <ProjectBanner title="Piano map" github="https://www.github.com/victoirebeaufils/pianomap"
+            description="A map of public pianos around the world based on user contributions" illustration={pianoMapIcon}
+            status="Work in progress"
+            date="March 26, 2020">
 
-      </ProjectBanner>
-      <div className="container">
-           <div className="row technologies">
-               <div className=""> <h5><span className="badge  badge-dark">MongoDB</span></h5> </div>
+        </ProjectBanner>
+        <div className="container">
+            <div className="row technologies">
+                <div className=""> <h5><span className="badge  badge-dark">MongoDB</span></h5> </div>
                 <div className=""> <h5><span className="badge  badge-dark">Express</span></h5></div>
                 <div className=""> <h5><span className="badge  badge-dark">Node.js</span></h5></div>
                 <div className=""> <h5><span className="badge  badge-dark">React</span></h5></div>
                 <div className=""> <h5><span className="badge  badge-dark">Maps API</span></h5></div>
 
-          
+
 
             </div>
             <div className="row product-long-description">
                 <h2>Overview</h2>
-            <p>I wanted to create a map on piano based on user contributions, creating a CRUD application with user profiles, contributions and a login form. </p>
-            <input type="checkbox"/><p> Made a mockup in Adobe XD </p>
+                <p>I wanted to create a map on piano based on user contributions, creating a CRUD application with user profiles, contributions and a login form. </p>
 
+                <div class="step">
+                    <div>
+                        <div class="circle">1</div>
+                        <div class="line"></div>
+                    </div>
+                    <div>
+                        <div class="title">I made a Mockup of the website with Adobe XD</div>
+                        <div class="body">
+
+                            <div class="img-container">
+                                <div class="row">
+                                    <div class="col-lg-6">
+                                    <img src={mockup1} class="img-responsive "></img>
+                                    </div>
+                                    <div class="col-lg-6">
+                                <img src={mockup2} class="img-responsive col-lg-6"></img>
+                                </div>
+                                </div>
+                                <div class="row">
+                                <div class="col-lg-6">
+                                    <img src={mockup3} class="img-responsive col"></img>
+                              </div>
+                              <div class="col-lg-6">
+                                    <img src={mockup4} class="img-responsive col"></img>
+                              </div>
+                                </div>
+                            </div>
+
+
+                        </div>
+                    </div>
+                    </div>
+                    <div class="step">
+                        <div>
+                            <div class="circle">2</div>
+                            <div class="line"></div>
+                        </div>
+                        <div>
+                        <div class="title"> What's next? </div>
+                        <div class="body">
+                            <h6> I started working on the backend (MERN Stack)</h6>
+                            <a> <input type="checkbox" class="form-check-input" checked ></input> Created routes, Middleware and Models for places and users <br/></a>
+                            <a> <input type="checkbox" class="form-check-input" checked></input> Created the frontend for pages above and extra pages for user profiles<br/> </a>
+                            <a> <input type="checkbox" class="form-check-input" ></input> Finished the authentification system using jwt <br/></a>
+                            
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
             </div>
-    
-</div>
-   
-  </Layout>)
 
-  export default PianoMapPage;
+    </Layout>)
+
+export default PianoMapPage;
