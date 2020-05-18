8d7eb9f0bb0c75a12ff07407f1d363f453a038c2
Victoire Beaufils <victoire.beaufil@gmail.com>
2020-05-18 22:27:31 +0200

    first commit

---
+++ src/pages/projects/mizu.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Layout from '../../components/layout/layout'
+
+const Mizu = (props)=> {
+return(
+<Layout>
+<ProjectBanner title="Piano map" github="https://www.github.com/victoirebeaufils/pianomap"
+            description="A map of public pianos around the world based on user contributions" illustration={pianoMapIcon}
+            status="Work in progress"
+            date="March 26, 2020">
+
+        </ProjectBanner>
+        
+</Layout>
+)
+}
+
+export default Mizu;
