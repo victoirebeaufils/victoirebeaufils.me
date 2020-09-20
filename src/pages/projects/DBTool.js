493f83c71b1436dc6d22c1bb00c6e8b7995108ea
Victoire <victoire.beaufil@gmail.com>
2020-09-20 14:24:02 -0400

    Changed DBTool page

--- src/pages/projects/DBTool.js
+++ src/pages/projects/DBTool.js
@@ -6,12 +6,11 @@ import ProjectBanner from "../../components/project-banner/project-banner"
 
 const DBToolPage = () => (
   <Layout>
-  {/*}  <ProjectBanner title="20-20-20" github="https://www.github.com/victoirebeaufils/20-20-20" date="December 30, 2019"
-      description="A React Native app to reduce eye strain following the 20-20-20 rule : Every 20 minutes, look 20 feet in the distance for 20 seconds"
-      illustration={} status="Incomplete">
+  {  <ProjectBanner title="DBTool" github="https://github.com/victoirebeaufils/dbtoolral Therapy (DBT)"
+  status="Incomplete">
 
     </ProjectBanner>
-*/}    <div className="container">
+}    <div className="container">
       <br />
       <hr />
 
@@ -22,79 +21,10 @@ const DBToolPage = () => (
         </div>
       </div>
       <br />
-      
-      <div className="row product-long-description">
-        <h2>Overview</h2>
-        <p>I wanted to create an app baased on Dialectic and Behavioral Therapy (DBT). I provided information about the 4 modules and skills used in DBT and  </p>
+      <iframe width="207" height="500" src="https://xd.adobe.com/embed/c908592a-2a8c-4845-6a4f-901d0a1961c1-bf39/" frameborder="0" allowfullscreen></iframe>
+     
 
-        <div class="step">
-          <div>
-            <div class="circle">1</div>
-            <div class="line"></div>
-          </div>
-          <div>
-            <div class="title">I made a Mockup of the website with Adobe XD</div>
-            <div class="body">
-
-            <div class="img-container">
-                <div class="row">
-                  <div class="col-lg-6">
-                   {/* <img src={} class="img-responsive "></img> */}
-                  </div>
-                  <div class="col-lg-6">
-                   {/* <img src={} class="img-responsive"></img> */}
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="col-lg-6">
-                  {/*  <img src={} class="img-responsive "></img> */}
-                  </div>
-                  <div class="col-lg-6">
-                   {/* <img src={} class="img-responsive "></img> */}
-                  </div>
-                  <div class="row">
-                    <div class="col">
-                     {/* <img src={} class="img-responsive "></img>*/}
-
-                    </div>
-                  </div>
-                </div>
-
-
-            </div>
-          </div>
-        </div>
-        </div>
-        <div class="step">
-          <div>
-            <div class="circle">2</div>
-            <div class="line"></div>
-          </div>
-          <div>
-            <div class="title"> What's next? </div>
-            <div class="body">
-              <h6> I started working on the backend (MERN Stack)</h6>
-              <a> <input type="checkbox" class="form-check-input" checked ></input> Created all screens <br /></a>
-              <a> <input type="checkbox" class="form-check-input" checked></input> Made a timer<br /> </a>
-              <a> <input type="checkbox" class="form-check-input" ></input> Set up push notifications  <br /></a>
-              <a> <input type="checkbox" class="form-check-input" ></input> Find a way to make the alarm ring  <br /></a>
-              <hr/>
-              <h6> Extra user experience features</h6>
-              <a> <input type="checkbox" class="form-check-input"  ></input>Modify timer circle so that its contour dimimishes as seconds pass <br /></a>
-              <a> <input type="checkbox" class="form-check-input"  ></input>Take the user directly to the next page after the timer is done 
-              instead of having them click a button <br /></a>
-
-              <hr/>
-              <h6> Features that are way out of the scope of this project but could be implemented in others </h6>
-              <a> <input type="checkbox" class="form-check-input"  ></input>Modify timer circle so that its contour dimimishes as seconds pass <br /></a>
-              <a> <input type="checkbox" class="form-check-input"  ></input>Take the user directly to the next page after the timer is done 
-              instead of having them click a button <br /></a>
-            </div>
-          </div>
-        </div>
-
-      </div>
-    </div>
+     </div>
 
 
   </Layout>)
